7fb991e3db525350caa40d2269c1e8172b1c082a
JoeJoel <46153914+JoeJoyal@users.noreply.github.com>
2019-01-13 10:38:33 +0530

    Initial commits from Find the element Present or Not sln_3

---
+++ Find_the _element_Present_or_Not_2.py
@@ -0,0 +1,18 @@
+#Find the element weather, present or Not
+#Using flag True/False
+#Search element declare
+#a = [4,9,6,2,0]
+#Input = array
+#Output = 4 True 
+
+def Find_the_element(a,Search_element):
+    for i in a:
+        flag = False
+        if(i==Search_element):
+            flag = True
+            break
+    return flag
+
+a = [4,9,6,2,0]
+Search_element = input('Enter the search element :')
+print(Find_the_element(a,Search_element))
